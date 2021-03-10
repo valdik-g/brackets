f5efd1a40db5e98371d265e5e25cbfe17b6c8fe7
valdik-g <77793654+valdik-g@users.noreply.github.com>
2021-03-10 13:34:32 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
     let arr = str.split("");
     let t = 0;
-    let k1,k2,k3,k4;
+    let k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12;
     let r = arr.join("");
     for(let i = 0;i<10000;i++) {
         k1 = arr.join("").indexOf("[]",0);
         k2 = arr.join("").indexOf("{}",0);
         k3 = arr.join("").indexOf("()",0);
         k4 = arr.join("").indexOf("||",0);
+        k5 = arr.join("").indexOf("11",0);
+        k6 = arr.join("").indexOf("22",0);
+        k7 = arr.join("").indexOf("33",0);
+        k8 = arr.join("").indexOf("44",0);
+        k9 = arr.join("").indexOf("55",0);
+        k10 = arr.join("").indexOf("66",0);
+        k11 = arr.join("").indexOf("77",0);
+        k12 = arr.join("").indexOf("88",0);
         let r1 = arr.join("");
         if (k1 !== -1) {
             arr.splice(k1,2);
@@ -29,14 +37,52 @@ module.exports = function check(str, bracketsConfig) {
             t++;
             continue;
         }
+        if (k5 !== -1) {
+            arr.splice(k5,2);
+            t++;
+            continue;
+        }
+        if (k6 !== -1) {
+            arr.splice(k6,2);
+            t++;
+            continue;
+        }
+        if (k7 !== -1) {
+            arr.splice(k7,2);
+            t++;
+            continue;
+        }
+        if (k8 !== -1) {
+            arr.splice(k8,2);
+            t++;
+            continue;
+        }
+        if (k9 !== -1) {
+            arr.splice(k9,2);
+            t++;
+            continue;
+        }
+        if (k10 !== -1) {
+            arr.splice(k10,2);
+            t++;
+            continue;
+        }
+        if (k11 !== -1) {
+            arr.splice(k11,2);
+            t++;
+            continue;
+        }
+        if (k12 !== -1) {
+            arr.splice(k12,2);
+            t++;
+            continue;
+        }
         if(r1.length === 0)
         {
             return true;
-            break;
         }
         if(t === 0) {
             return false;
-            break;
         }
         t = 0;
     }
